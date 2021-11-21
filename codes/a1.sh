bf17da820c6454f9d8b17c590c088fe57d5c0a99
rdchip <dario9130@hotmail.com>
2021-11-20 18:02:50 -0800

    Update a1.sh

--- codes/a1.sh
+++ codes/a1.sh
@@ -1,2 +1,2 @@
-echo "plotting"; python tempsensor2.py
-sleep 3; pidof python tempsensor2.py > pid.txt; echo "sleeping"; sleep 24h; echo "killing"; pid=$(cat pid.txt) && kill -9 $pid; dphys-swapfile swapoff; dphys-swapfile swapon
+echo "plotting"; python3 tempsensor2.py
+sleep 3; pidof python3 tempsensor2.py > pid.txt; echo "sleeping"; sleep 24h; echo "killing"; pid=$(cat pid.txt) && kill -9 $pid; dphys-swapfile swapoff; dphys-swapfile swapon
