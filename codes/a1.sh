03bc7f6e69c10691922417acf7cb60a0bcc36106
rdchip <dario9130@hotmail.com>
2021-12-24 11:13:21 -0800

    Delete a1.sh

--- codes/a1.sh
+++
@@ -1,2 +0,0 @@
-echo "plotting"; python3 tempsensor2.py
-sleep 3; pidof python3 tempsensor2.py > pid.txt; echo "sleeping"; sleep 24h; echo "killing"; pid=$(cat pid.txt) && kill -9 $pid; dphys-swapfile swapoff; dphys-swapfile swapon
