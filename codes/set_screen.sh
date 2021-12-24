a2a46c5f4b48b879cd45d0b00cf3ae44d57aa17d
rdchip <dario9130@hotmail.com>
2021-12-24 11:13:42 -0800

    Delete set_screen.sh

--- codes/set_screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# install the touch screen
-#chmod +x set_screen.sh
-
-cd /home/pi/Raspberry/MZDPI/vga
-sudo chmod +x mzdpi-vga-autoinstall-offline
-sudo ./mzdpi-vga-autoinstall-offline
-
-#$ ./set_screen.sh
