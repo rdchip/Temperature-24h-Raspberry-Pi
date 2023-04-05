ac5789b8adde9c634e39c9a61db70324504c85e5
rdchip <dario9130@hotmail.com>
2023-04-04 23:33:45 -0400

    Update temp.sh

--- codes/MZDPI/temp.sh
+++ codes/MZDPI/temp.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# install the touch screen
 #chmod +x temp.sh
-
+cd /home/pi/Raspberry/
 while :
 do
-    parallel -j 2 < a1.sh
+    python3 temp-plot.py &
+    sleep 30
+    pidof python3 temp-plot.py > pid.txt
+    cat pid.txt
+    echo "plotting 24h"
+    sleep 24h
+    pid=$(cat pid.txt) && kill -9 $pid
+    echo "killing"
+    sleep 10
+    dphys-swapfile swapoff
+    sleep 3
+    dphys-swapfile swapon
 done
 #$ sudo ./temp.sh
